0b5b0cddbf27a5fbced4860a71b72b133f4e050f
EzviD <bboyezvid01@gmail.com>
2020-01-16 15:29:16 +0200

    add facult rector send_img

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
 import telebot as tb
-from resources.parser import get_text, news_parse
+from random import randint,seed
+from resources.parser import get_text, news_parse, f_parse, r_parse
 
+#CONFIG
 Token = "966190262:AAEJJpUkyV_AwbKV0HSjXb4qNuYxVv937GE"
 url = "https://www.lnu.edu.ua/"
+seed(1)
 bot = tb.TeleBot(Token)
 
+#COMMANDS
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id,'''Hi there.
 It is LNU website in Telegram for your comfort.\n
-Type /help to start''')
+Enter /help to start''')
 
 @bot.message_handler(commands=['help'])
 def start_message(message):
     bot.send_message(message.chat.id,'''/help - list of commands,
 /news - last news
-/about - information about university''')
+/about - information about university
+/facult - list of faculties
+/rector - the current rector''')
 
 @bot.message_handler(commands=['news'])
 def news(message):
     url='https://www.lnu.edu.ua/news/all/'
     result = news_parse(url)
+
     if result is not None:
         _id=0
         for item in result:
@@ -32,10 +39,44 @@ def news(message):
 
 @bot.message_handler(commands=['about'])
 def about(message):
-    with open("tmp/about.txt","r") as input:
+    with open("tmp/about.txt","r",encoding='utf-8') as input:
         data = input.read()
         input.close()
     message_for_user = f"{data}\n{url}"
     bot.send_message(message.chat.id, message_for_user)
+
+@bot.message_handler(commands=['facult'])
+def facult(message):
+    url = "https://www.lnu.edu.ua/about/faculties/"
+    result = f_parse(url)
+
+    _id=0
+    for item in result:
+        message_for_user = f'''{item[f'logo{_id}']}\n
+    Адреса: {item[f'text{_id}'][0]}
+    Телефон: {item[f'text{_id}'][1]}
+    E-mail: {item[f'text{_id}'][2]}
+    Сайт: {item[f'text{_id}'][3]}'''
+        bot.send_message(message.chat.id, message_for_user)
+        _id+=1
+
+@bot.message_handler(commands=['rector'])
+def rector(message):
+    url = "https://www.lnu.edu.ua/about/administration/rector/"
+    result = r_parse(url)
+
+    message_for_user = f'''{result[0]['name']}\n
+{result[0]['phone']}
+E-mail: {result[0]['email']}\n
+Стратегія розвитку:
+{result[0]['strat']}'''
+    bot.send_message(message.chat.id, message_for_user)
+
+#CONTENT TYPES
+@bot.message_handler(content_types=['text'])
+def send_img(message):
+    if message.text.lower() in ['lnu','лну']:
+        photo = open(f'tmp/img/lnu/{randint(1,3)}.jpg','rb')
+        bot.send_photo(message.chat.id, photo)
 
 bot.polling()
--- resources/parser.py
+++ resources/parser.py
@@ -10,18 +10,51 @@ def get_text(url):
 def news_parse(url):
     results = []
     text = get_text(url)
-
     soup = BeautifulSoup(text,features="lxml")
+
     sections = soup.find_all('section',{'class':['post with-image clearfix','post no-image clearfix']})
     if sections:
-        id=0
+        _id=0
         for item in sections:
             post = item.find('h2', {'class': 'post-title'})
-            results.append({f'text{id}':post.text,
-                            f'href{id}':post.find('a').get('href'),
-                            f'date{id}':item.find('div',{'class':'post-date'}).text})
-            id+=1
+            results.append({f'text{_id}':post.text,
+                            f'href{_id}':post.find('a').get('href'),
+                            f'date{_id}':item.find('div',{'class':'post-date'}).text})
+            _id+=1
     else:
         results.append('There are no such news')
+
+    return results
+
+#PARSER FOR FACULTIES
+def f_parse(url):
+    results = []
+    text = get_text(url)
+    soup = BeautifulSoup(text,features="lxml")
+
+    sections = soup.find_all('li',{'class':'clearfix'})
+    if sections:
+        _id=0
+        for item in sections:
+            post = item.find('div',{'class':'details'}).find_all('p')
+            results.append({f'logo{_id}':item.find('h2', {'class':'title'}).text,
+                            f'text{_id}':[x.find('span',{'class':'value'}).text for x in post]})
+            _id+=1
+    else:
+        results.append('There are no such faculties')
+
+    return results
+
+#RECTOR PARSER
+def r_parse(url):
+    results = []
+    text = get_text(url)
+    soup = BeautifulSoup(text,features="lxml")
+
+    section = soup.find('div',{'class':'vc_row wpb_row vc_row-fluid'})
+    results.append({'name':soup.find('h2',{'class':'name'}).find('strong').text,
+                    'phone':soup.find('p',{'class':'phone'}).text,
+                    'email':soup.find('p',{'class':'email'}).find('a').text,
+                    'strat':section.find_all('a',{'rel':'noopener noreferrer'})[1].get('href')})
 
     return results
